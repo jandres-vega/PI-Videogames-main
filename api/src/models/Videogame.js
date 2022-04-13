06b312678cdbc82ea33b150e78cdd2f165a2fbfc
Andres vega <jandresvega@itsa.edu.co>
2022-04-13 08:24:41 -0500

    agregando modelos a nuestra base de datos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,41 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    release_date: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    background_image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createDateBase: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+
   });
 };
